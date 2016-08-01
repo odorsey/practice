8388c918b8dfcdc7bf958177ed18143ff3289fe4
odorsey <oddorsey@gmail.com>
2016-08-01 19:07:59 -0400

    Improved strikethrough functionality

--- javascript/todo/js/todo.js
+++ javascript/todo/js/todo.js
@@ -11,9 +11,10 @@ function storeTodos () {
     return items;
 }
 
-// Function to add some todo items
-document.getElementById('add').addEventListener('click', add);
 listTodos();
+
+// Function to add some todo items upon click of #add
+document.getElementById('add').addEventListener('click', add);
 
 function add() {
   var task = document.getElementById('entry').value;
@@ -23,7 +24,7 @@ function add() {
       localStorage.setItem('todo', JSON.stringify(items));
 
       listTodos();
-      form.reset();
+      document.getElementById('createList').reset();
       return false;
     } else {
       alert("Please add a todo item to your list.");
@@ -36,32 +37,48 @@ function listTodos() {
 
     var html = '<ul>';
     for (i = 0; i < items.length; i++){
-      html += '<li class="todoItem">' + items[i] + '</li>';
+      html += '<li><span class="todoItem">' + items[i] + '</span><a href="#" class="deleteItems"> x</a>' + '</li>';
     };
     html += '</ul>';
 
     document.getElementById('items').innerHTML = html;
+    var todoItem = document.getElementsByClassName('todoItem');
+    var clicked = false;
+
+    // loop through all items in the array and add the event listener
+    for (i = 0; i < todoItem.length; i++) {
+      todoItem[i].addEventListener('click', clickhandler);
+      // Set id to uniquely identify each todo item
+      todoItem[i].id = 'todoItem-' + i;
+      id = todoItem[i].id;
+      //console.log(id);
+      if (localStorage.getItem('status') === id) {
+        document.getElementById(id).className += " strikethrough";
+        //console.log(localStorage.getItem('status'));
+      }
+    }
+
+    // Clickhandler function sets class to strikethrough if todo item is clicked
+    function clickhandler(id){
+      var clicked = true;
+      if (clicked) {
+        id = id.target.id;
+        document.getElementById(id).className += " strikethrough";
+        localStorage.setItem('status', id);
+      }
+    }
 }
 
-// Setting up functionality to strikethrough to-do items if finished
-var todoItem = document.getElementsByClassName('todoItem');
-
-// Set clicked status
-var clicked = false;
-
-// loop through all items in the array and add the event listener
-for (i = 0; i < todoItem.length; i++) {
-  todoItem[i].addEventListener('click', clickhandler);
-  // Set id to uniquely identify each todo item
-  todoItem[i].id = 'todoItem-' + i;
-  id = todoItem[i].id;
+// Function to remove todo items if "x" is clicked
+var deleteItems = document.getElementsByClassName('deleteItems');
+for (i = 0; i < deleteItems.length; i++) {
+  deleteItems[i].addEventListener('click', remove);
 }
 
-function clickhandler(id){
+function remove(deleteItems) {
   var clicked = true;
   if (clicked) {
-    id = id.target.id;
-    document.getElementById(id).className += " strikethrough";
+    console.log("The x of " + id + " was clicked!");
     return false;
   }
 }
